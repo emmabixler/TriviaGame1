ba2a2ca44e011c137a47fee1f9ba08d5acba4a7d
leb <leb5313@gmail.com>
2019-02-19 20:45:13 -0500

    switched files from TriviaGame to TriviaGame1 due to git url fail

---
+++ assets/javascript/app1.js
@@ -0,0 +1,114 @@
+var wins = 0;
+var incorrect = 0;
+var results = 0;
+
+var questionQuiz = [
+  {
+    question:
+      " 1.Game where player guesses spins a wheel to  guess a letter of the hidden phrase",
+    answers: {
+      a: "Wheel of Fortune",
+      b: "Family Feud",
+      c: "Price is Right",
+      d: "Who Wants to Be a Millionaire"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: " 2. Game where the constants answer in a form of a question",
+    answers: {
+      a: "Family Feud",
+      b: "Price is Right",
+      c: "Who Wants to Be a Millionaire",
+      d: "Jeopardy"
+    },
+    correctAnswer: "d"
+  },
+  {
+    question:
+      " 3.Trivia Game app that you can play with friends that also you to spin a colorful wheel",
+    answers: {
+      a: "Family Feud",
+      b: "Candy Crush",
+      c: "Trivia Crack",
+      d: "Jackbox"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question:
+      " 4. Two teams that consist of 5 players that guess top answers of a stated quesitons",
+    answers: {
+      a: "Family Feud",
+      b: "Candy Crush",
+      c: "Trivia Crack",
+      d: "Jackbox"
+    },
+    correctAnswer: "a"
+  },
+  {
+    questions:
+      " 5. Game where the constants guess the prices of of retail items and also to see Drew Carey",
+    answers: {
+      a: "Let's Make a Deal",
+      b: "Deal or No Deal",
+      c: "Price is Right",
+      d: "Press Your Luck"
+    },
+    correctAnswer: "c"
+  },
+  {
+    questions: "6. 14 questions with multiple choices and lifelines",
+    answers: {
+      a: "Let's Make a Deal",
+      b: "Who Wants to Be A Millionaire ",
+      c: "Price is Right",
+      d: "Press Your Luck"
+    },
+    correctAnswer: "b"
+  },
+  {
+    questions: "7. Game where constants picks from 26 sealed cases",
+    answers: {
+      a: "Let's Make a Deal",
+      b: "Who Wants to Be A Millionaire ",
+      c: "Price is Right",
+      d: "Press Your Luck"
+    },
+    correctAnswer: "a"
+  },
+
+  {
+    questions:
+      "8. Constants answers trivia questions for a chance to not get Whammy!",
+    answers: {
+      a: "Let's Make a Deal",
+      b: "Who Wants to Be A Millionaire ",
+      c: "Price is Right",
+      d: "Press Your Luck"
+    },
+    correctAnswer: "d"
+  },
+  {
+    questions: "9. Constants go to dress up and to make trades with the host",
+    answers: {
+      a: "Let's Make a Deal",
+      b: "Who Wants to Be A Millionaire ",
+      c: "Price is Right",
+      d: "Press Your Luck"
+    },
+    correctAnswer: "a"
+  },
+
+  {
+    questions:
+      "10. Given a mystery word, two teams of two, guesses what the word is and goes up in this scheme",
+    answers: {
+      a: "Let's Make a Deal",
+      b: "Who Wants to Be A Millionaire ",
+      c: "The $100,000 Pyramid",
+      d: "Press Your Luck"
+    },
+    correctAnswer: "a"
+  }
+];
